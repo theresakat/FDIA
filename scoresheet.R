69d6fb21893068eca4df96c5e23b9a73d9de8898
theresakat <theresa.burcsu@oregon.gov>
2018-05-16 15:26:29 -0700

    Mediocre barplot created.

--- scoresheet.R
+++ scoresheet.R
@@ -3,7 +3,7 @@
 # Purpose: this script is used to create the scoresheet tally of data elements by theme
 # 
 # Required files:
-#   dat   contains the contents of the 1_tblFrameworkData_MASTER.csv
+#   dat   contains the contents of the 1_tblFrameworkData_MASTER.csv (load using 1_load_FDIA.R)
 #   x     contains the raw imported survey data
 
 # Workflow
@@ -14,13 +14,13 @@
 #   apply this script
 
 # Create empty data frame
-de<- data.frame(ID=c(1:nrow(surveyData)))
+de <- data.frame(ID=c(1:nrow(surveyData)))
 
 # Add survey responses for data element names and IDs to the data frame 
 de<-data.frame(surveyData[,10:11])
 names(de)<-c("DataElem","ID")
 
-# Merge the Framework theme info and the survey data responses
+# Merge the survey responses and Framework theme info
 y<-merge(de,dat, all.x=TRUE)
 
 # Create a selection vector to identify the unmatched survey responses
@@ -43,9 +43,31 @@ for(i in corrections$DataElem) {
 surveyData[which(is.na(surveyData$V11)),10:11]
 
 # Construct the Scoresheet tallies:
-# Count the expected number of elements per theme
-# Inner join themes with the corrected survey data by ID
-# Count the observed number of elements per theme
-# Calculate the percentage of surveys returned (observed/expected)
-# Create bar charts showing observed and expected
 
+# Inner join themes with the corrected survey data by ID
+surveyDataThm <- merge(surveyData, dat, by.x = "V11", by.y = "ID") 
+
+# Count the expected number of elements per theme
+expected<-with(dat, table(Theme))
+
+# Count the observed number of elements per theme
+observed<-as.numeric(with(surveyDataThm, table(Theme)))
+# observed<-data.frame(with(surveyDataThm, table(Theme)))
+
+# Calculate the percentage of surveys returned (observed/expected)
+retrate<-observed[,2]/expected[,2]
+diff<-expected-observed
+tally<-cbind(observed,expected,diff,retrate)
+
+# Create bar charts showing observed and expected
+tallyplt<-spineplot(tally[,c(1,3)],
+          main = "Data Elements Surveyed Relative to Theme",
+          xlab = "", ylab = "",
+          yaxlabels = c("Observed","Expected"),
+          off = 10,
+          col = c("steelblue3","bisque"))
+text(tallyplt,tallydf$retrate+2,labels=as.character(tallydf$retrate))
+
+library(ggplot2)
+
+tallyplt<-ggplot() + geom_bar(aes(y = observed),data=tallydf, stat = "identity");tallyplt
